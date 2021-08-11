8d017b507c5bc8a4e16b4af8840b2db8494fdebc
Dheerajsingh <dheerajkumarblr@gmail.com>
2021-08-11 21:26:48 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode:'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')],
+}

988c930112476ede03365174dbc0388df46efc19
Dheerajsingh <dheerajkumarblr@gmail.com>
2021-08-11 21:38:13 +0530

    chnges made

--- components/Banner.js
+++ components/Banner.js
@@ -11,7 +11,7 @@ function Banner(){
           />
           <div className="absolute top-1/2 w-full text-center">
               <p className="text-sm sm:text-lg" >Not Sure Where to go? Perfect.</p>
-              <button className="bg-white text-purple-500 my-3 px-10 py-4 shadow-md rounded-full font-bold hover:shadow-xl active:scale-90 transition duration-150" >I"m flexible</button>
+              <button className="bg-white text-purple-500 my-3 px-10 py-4 shadow-md rounded-full font-bold hover:shadow-xl active:scale-90 transition duration-150" >I am flexible</button>
           </div>
         </div>
         </>

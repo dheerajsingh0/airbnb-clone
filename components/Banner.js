4baa2d62a015fe7680a419a2832a67c1333b93f3
Dheerajsingh <dheerajkumarblr@gmail.com>
2021-08-14 18:47:11 +0530

    updated

--- components/Banner.js
+++ components/Banner.js
@@ -3,13 +3,13 @@ function Banner(){
     return(
         <>
         <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] ">
-          <Image
+          <Image 
           src="/Banner.webp"
           layout="fill"
           objectFit="cover"
           alt="Banner pic"
           />
-          <div className="absolute top-1/2 w-full text-center">
+          <div className=" absolute top-1/2 w-full text-center">
               <p className="text-sm sm:text-lg" >Not Sure Where to go? Perfect.</p>
               <button className="bg-white text-purple-500 my-3 px-10 py-4 shadow-md rounded-full font-bold hover:shadow-xl active:scale-90 transition duration-150" >I am flexible</button>
           </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 import "../styles/globals.css"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header/>
+    <Component {...pageProps} />
+    <Footer/>
+    </>
+    ) 
 }
 
 export default MyApp
